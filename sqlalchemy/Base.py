7fd3cc77f442e0163d932f0bd51d0a0444661134
DemonicCat <36911685+DemonicCat@users.noreply.github.com>
2018-07-06 15:16:22 +0800

    Update Base.py

--- sqlalchemy/Base.py
+++ sqlalchemy/Base.py
@@ -3,7 +3,7 @@
 
 from sqlalchemy import Column, Integer, BigInteger, SmallInteger, String,\
                         DateTime, Text, Date, DECIMAL, Float, Time, create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
 # 创建对象的基类:
@@ -20,10 +20,21 @@ class User(Base):
 
 # 初始化数据库连接:
 engine = create_engine('mysql+mysqlconnector://root:root@192.168.1.13:3306/test')
-# 创建DBSession类型:
-DBSession = sessionmaker(bind=engine)
 
-session = DBSession()
+# 创建DBSession类型:
+Session = sessionmaker(bind=engine)
+
+'''
+用scoped_session的目的主要是为了线程安全。
+scoped_session类似单例模式，当我们调用使用的时候，会先在Registry里找找之前是否已经创建session了。
+要是有，就把这个session返回。
+要是没有，就创建新的session，注册到Registry中以便下次返回给调用者。
+这样就实现了这样一个目的：在同一个线程中，call scoped_session 的时候，返回的是同一个对象
+'''
+#session_factory = sessionmaker(bind=some_engine)
+#Session = scoped_session(session_factory)
+
+session = Session()
 ed_user = User(name='ed')
 session.add(ed_user)
 session.commit()
