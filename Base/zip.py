73606daaa45fb065505430691c11c8aed8e91a01
DemonicCat <36911685+DemonicCat@users.noreply.github.com>
2018-08-15 15:33:31 +0800

    Update zip.py

--- Base/zip.py
+++ Base/zip.py
@@ -1,2 +1,16 @@
+
 import zipfile
 from io import BytesIO
+
+file_list = ['E:\\code\\test.py', 'E:\\code\\2\\2.jpg']
+
+memory_file = BytesIO()
+with zipfile.ZipFile(memory_file, "w", zipfile.ZIP_DEFLATED) as zf:
+    for _file in file_list:
+        with open( _file, 'rb') as fp:
+            file_name = _file.split('/')[-1]
+            zf.writestr(file_name, fp.read())
+memory_file.seek(0)
+
+with open('E:\\code\\12.zip', 'wb') as f:
+    f.write(memory_file.getvalue())
